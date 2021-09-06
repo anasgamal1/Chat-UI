36bf2373e18f9918ed0fd65cc201bda2a66888a4
AnasGamal <anas.gamal@izam.co>
2021-09-06 14:41:28 +0200

    I have a qustion here.

--- lib/chat_room.dart
+++ lib/chat_room.dart
@@ -11,12 +11,53 @@ class ChatRoom extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              height: 350,
+              padding: EdgeInsets.all(25),
+              height: 320,
+              child: Column(
+                children: [
+                  Text(
+                    'Chat with \nfriends',
+                    style: TextStyle(
+                      fontSize: 37,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    child: ListView.builder(
+                      itemCount: 6,
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemBuilder: (_, index) {
+                        if (index == 0) {
+                          return CircleAvatar(
+                            radius: 35,
+                            backgroundColor: Colors.green[500],
+                            child: Icon(
+                              Icons.search,
+                              size: 35,
+                              color: Colors.white,
+                            ),
+                          );
+                        }
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            radius: 35,
+                            backgroundColor: Colors.green[500],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
             Expanded(
                 child: Container(
               decoration: BoxDecoration(
-                color: Colors.red,
+                color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20),
                   topRight: Radius.circular(20),
